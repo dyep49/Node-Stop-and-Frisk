880e7c50d6484fb44918fe49ce12dc82ad29533d
dyep49 <dyep49@gmail.com>
2014-02-21 13:11:34 -0500

    added styles

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -5,41 +5,20 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-    var projection = d3.geo.mercator()
-  					.center([-73.94, 40.70])
-  					.scale(90000)
-  					.translate([(width) / 2, (height)/2]);
+var projection = d3.geo.mercator()
+				.center([-73.94, 40.70])
+				.scale(90000)
+				.translate([(width) / 2, (height)/2]);
 
-	var path = d3.geo.path()
-			.projection(projection);
+var path = d3.geo.path()
+		.projection(projection);
 
-	var g = svg.append("g");
+var g = svg.append("g");
 
-	g.append("g")
-		.attr("id", "boroughs")
-		.selectAll(".state")
-		.data(nyb.features)
-		.enter().append("path")
-		.attr("class", function(d){ return d.properties.name; })
-		.attr("d", path);
-
-
-
-// var width = 960;
-// var height = 500;
-
-// var svg = d3.select('body').append('svg')
-//     .attr('width', width)
-//     .attr('height', height);
-
-// var projection = d3.geo.albersUsa()
-//     .scale(1000)
-//     .translate([width / 2, height / 2]); 
-
-// var path = d3.geo.path()
-//     .projection(projection);
-
-//     svg.append('path')
-//         .datum(topojson.feature(states, states.objects.state))
-//         .attr('class', 'states') // defined in CSS
-//         .attr('d', path);
+g.append("g")
+	.attr("id", "boroughs")
+	.selectAll(".state")
+	.data(nyb.features)
+	.enter().append("path")
+	.attr("class", function(d){ return d.properties.name; })
+	.attr("d", path);
