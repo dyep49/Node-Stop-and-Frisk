ae5e352429fdf2edf32c97c4deed42e77939b482
dyep49 <dyep49@gmail.com>
2014-02-21 12:23:13 -0500

    made shit happen

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,12 +1,49 @@
-var svg_width = 1000;
-var svg_height = 500;
+var width = 960,
+   height = 500;
 
-d3.select('body')
-		.append('svg')
-		.attr('width', svg_width+'px')
-		.attr('height', svg_height+'px')
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var nyb = 
+
+  	console.log(nyb)
+
+    var projection = d3.geo.mercator()
+  					.center([-73.94, 40.70])
+  					.scale(50000)
+  					.translate([(width) / 2, (height)/2]);
+
+	var path = d3.geo.path()
+			.projection(projection);
+
+	var g = svg.append("g");
+
+	g.append("g")
+		.attr("id", "boroughs")
+		.selectAll(".state")
+		.data(nyb.features)
+		.enter().append("path")
+		.attr("class", function(d){ return d.properties.name; })
+		.attr("d", path);
 
 
-d3.json("states", function(error, us){
-	console.log(us);
-});
+
+// var width = 960;
+// var height = 500;
+
+// var svg = d3.select('body').append('svg')
+//     .attr('width', width)
+//     .attr('height', height);
+
+// var projection = d3.geo.albersUsa()
+//     .scale(1000)
+//     .translate([width / 2, height / 2]); 
+
+// var path = d3.geo.path()
+//     .projection(projection);
+
+//     svg.append('path')
+//         .datum(topojson.feature(states, states.objects.state))
+//         .attr('class', 'states') // defined in CSS
+//         .attr('d', path);
